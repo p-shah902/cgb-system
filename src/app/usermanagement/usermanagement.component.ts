82d8a59f54c9e2e48737f23ee328d9693eab28f4
Prinkesh <prinkesh.patel@smbsolutionkwt.com>
2025-03-03 22:31:03 +0530

    design

--- src/app/usermanagement/usermanagement.component.ts
+++ src/app/usermanagement/usermanagement.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-usermanagement',
   standalone: true,
-  imports: [],
+  imports: [NgbDropdownModule],
   templateUrl: './usermanagement.component.html',
   styleUrl: './usermanagement.component.scss'
 })
