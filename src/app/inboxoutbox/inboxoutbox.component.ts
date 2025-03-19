9224c73b8f71a0ef4ab1c8ddd8111acaae534c87
Prinkesh <prinkesh.patel@smbsolutionkwt.com>
2025-03-19 10:17:40 +0530

    design

--- src/app/inboxoutbox/inboxoutbox.component.ts
+++ src/app/inboxoutbox/inboxoutbox.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-inboxoutbox',
   standalone: true,
-  imports: [],
+  imports: [NgbNavModule],
   templateUrl: './inboxoutbox.component.html',
   styleUrl: './inboxoutbox.component.scss'
 })
 export class InboxoutboxComponent {
-
+  active = 1;
 }
