c87546fb9d31aab1a3dcddd36306aa4889d3b726
Priyansh Sha <priyansh@mamotechnolabs.com>
2025-03-26 15:56:50 +0530

    Role Updates

--- src/models/role.ts
+++ src/models/role.ts
@@ -1,57 +1,73 @@
 export interface ApiResponse<T> {
-    message: string;
-    errorMessages: any;
-    exception: any;
-    data: T;
-    status: boolean;
-  }
+  message: string;
+  errorMessages: any;
+  exception: any;
+  data: T;
+  status: boolean;
+}
 
+export interface Particular {
+  particularsId: number;
+  particularsName: string;
+  description: string;
+}
 
-  export interface Particular {
-    particularsId: number;
-    particularsName: string;
-    description: string;
-  }
-  
-  export interface ParticularType {
-    typeId: number;
-    typeName: string;
-    particulars: Particular[];
-  }
+export interface UserRoleAccesses {
+  particularId: number;
+  particularsName: string;
+  accessName: string,
+  accessId: number,
+  usersAceess: UserAccess[]
+}
 
-  export interface UpsertUserRolesPaylod{
-    roleId: number;
-    roleName: string;
-    description: string;
-    accessId: number[];
-    sectionId: number[] | null;
-    isReadAccess: boolean;
-    isWriteAccess: boolean;
-  }
+export interface UserAccess {
+  id: number,
+  particularId: number,
+  roleId: number,
+  description: string,
+  isReadAccess: boolean,
+  isWriteAccess: boolean
+}
 
-  export interface UserRole{
-    id: number;
-    name:string;
-    description:string;
-    createdDate:string;
-  }
+export interface ParticularType {
+  typeId: number;
+  typeName: string;
+  particulars: Particular[];
+}
 
-  export interface UserRoleAccess{
-    roleId:number;
-    roleName:string;
-    typeId: number;
-    typeName:string;
-    particularsId: number;
-    particularsName: string;
-    isWriteAccess: boolean;
-    isReadAccess: boolean;
-      
-  }
+export interface UpsertUserRolesPaylod {
+  roleId: number;
+  roleName: string;
+  description: string;
+  accessId: number[];
+  sectionId: number[] | null;
+  isReadAccess: boolean;
+  isWriteAccess: boolean;
+}
 
-  export interface AuthResponse{
-    token:string;
-    userRoleAccesses:UserRoleAccess[];
-    success:boolean;
-  }
+export interface UserRole {
+  id: number;
+  name: string;
+  description: string;
+  createdDate: string;
+}
 
-  // export 
+export interface UserRoleAccess {
+  roleId: number;
+  roleName: string;
+  typeId: number;
+  typeName: string;
+  particularsId: number;
+  particularsName: string;
+  isWriteAccess: boolean;
+  isReadAccess: boolean;
+
+}
+
+export interface AuthResponse {
+  token: string;
+  userRoleAccesses: UserRoleAccess[];
+  success: boolean;
+}
+
+// export
--- src/service/role.service.ts
+++ src/service/role.service.ts
@@ -1,8 +1,19 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, catchError, Observable, of, tap } from 'rxjs';
-import { ApiResponse, ParticularType, UpsertUserRolesPaylod, UserRole } from '../models/role';
-import { getUserParticularsListUri, getUserRolesUri, UpsertUserRolesUri } from '../utils/api/api';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, catchError, Observable, of, tap} from 'rxjs';
+import {
+  ApiResponse,
+  ParticularType,
+  UpsertUserRolesPaylod,
+  UserRole,
+  UserRoleAccesses
+} from '../models/role';
+import {
+  getAllRoleAccessListUri,
+  getUserParticularsListUri,
+  getUserRolesUri,
+  UpsertUserRolesUri
+} from '../utils/api/api';
 
 @Injectable({
   providedIn: 'root'
@@ -18,45 +29,58 @@ export class RoleService {
   private userRoleListSubject = new BehaviorSubject<UserRole[]>([]);
   userRoleList$ = this.userRoleListSubject.asObservable();
 
-  constructor(private http:HttpClient) { }
+  private userAccessListSubject = new BehaviorSubject<UserRoleAccesses[]>([]);
+  userAccessList$ = this.userAccessListSubject.asObservable();
 
-  getUserParticularsList(typeId:number):Observable<ApiResponse<ParticularType[]>>{
+  constructor(private http: HttpClient) {
+  }
+
+  getUserParticularsList(typeId: number): Observable<ApiResponse<ParticularType[]>> {
     return this.http.get<ApiResponse<ParticularType[]>>(`${getUserParticularsListUri}/${typeId}`)
-            .pipe(
-              tap(response=>{
-                if(response.status && response.data)
-                {
-                  this.userParticularsSubject.next(response.data);
-                }
-              })
-            );
+      .pipe(
+        tap(response => {
+          if (response.status && response.data) {
+            this.userParticularsSubject.next(response.data);
+          }
+        })
+      );
   }
 
-  upsertUserRoles(upsertPayload:UpsertUserRolesPaylod){
-    return this.http.post<any>(UpsertUserRolesUri,upsertPayload).pipe(
-          tap(response => {
-            console.log(response);
-            if (response && response.success) {
-
-              this.isUpsertUserRoleSubject.next(true);
-            }
-          }),
-          catchError(error => {
-            console.error(error);
-            return of({ success: false, message: error.error?.message || 'Error Accured' });
-          })
-        );
+  getAllRoleAccessList(): Observable<ApiResponse<UserRoleAccesses[]>> {
+    return this.http.get<ApiResponse<UserRoleAccesses[]>>(`${getAllRoleAccessListUri}`)
+      .pipe(
+        tap(response => {
+          if (response.status && response.data) {
+            this.userAccessListSubject.next(response.data);
+          }
+        })
+      );
   }
 
-  getUserRolesList():Observable<ApiResponse<UserRole[]>>{
+  upsertUserRoles(upsertPayload: UpsertUserRolesPaylod) {
+    return this.http.post<any>(UpsertUserRolesUri, upsertPayload).pipe(
+      tap(response => {
+        console.log(response);
+        if (response && response.success) {
+
+          this.isUpsertUserRoleSubject.next(true);
+        }
+      }),
+      catchError(error => {
+        console.error(error);
+        return of({success: false, message: error.error?.message || 'Error Accured'});
+      })
+    );
+  }
+
+  getUserRolesList(): Observable<ApiResponse<UserRole[]>> {
     return this.http.get<ApiResponse<UserRole[]>>(getUserRolesUri)
-            .pipe(
-              tap(response=>{
-                if(response.status && response.data)
-                {
-                  this.userRoleListSubject.next(response.data);
-                }
-              })
-            );
+      .pipe(
+        tap(response => {
+          if (response.status && response.data) {
+            this.userRoleListSubject.next(response.data);
+          }
+        })
+      );
   }
 }
