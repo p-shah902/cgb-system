917e0b63427600b2473f009171a307d3fb0dce34
Priyansh Sha <priyansh@mamotechnolabs.com>
2025-03-26 12:16:08 +0530

    Side bar

--- src/components/sidebar/sidebar.component.ts
+++ src/components/sidebar/sidebar.component.ts
@@ -1,18 +1,52 @@
-import { NgClass, NgIf } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {NgClass, NgForOf, NgIf} from '@angular/common';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {menuItems} from '../../models/menu';
+import {SafeHtmlDirective} from '../../directives/safe-html.directive';
+import {ActivatedRoute, Router, NavigationEnd} from '@angular/router';
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [NgClass, NgIf],
+  imports: [NgClass, NgIf, NgForOf, SafeHtmlDirective],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
+
 export class SidebarComponent {
   @Input() isExpanded: boolean = true;
   @Output() toggleSidebar: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   expandedMenus: { [key: string]: boolean } = {};
+  protected readonly menuItems = menuItems;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.updateCurrentPath();
+    });
+  }
+
+  updateCurrentPath(): void {
+    let menuItem = this.menuItems.find(f => f.path === this.getFullPath(this.activatedRoute) || (f.children || []).find(d => d.path === this.getFullPath(this.activatedRoute)));
+    if (menuItem?.title) {
+      this.toggleSubmenu(menuItem?.title);
+    }
+  }
+
+  getFullPath(route: ActivatedRoute): string {
+    let path = route.routeConfig?.path || '';
+
+    if (route.firstChild) {
+      const childPath = this.getFullPath(route.firstChild);
+      if (childPath) {
+        path += `/${childPath}`;
+      }
+    }
+
+    return path;
+  }
 
   handleSidebarToggle = () => {
     this.isExpanded = !this.isExpanded;
