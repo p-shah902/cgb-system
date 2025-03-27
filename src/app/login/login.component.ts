bb92449c2193e6964d2e3dfe3da21015837edc6d
SagarDesai10 <desaisagar8090@gmail.com>
2025-03-27 23:30:33 +0530

    add toast in login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,11 +2,14 @@ import {Component, inject, Inject} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {AuthService} from '../../service/auth.service';
 import {Router} from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ToastService } from '../../service/toast.service';
+import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule,NgbToastModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -17,12 +20,14 @@ export class LoginComponent {
   //   private router: Router
   // ) {}
 
-  authService = inject(AuthService);
-  router = inject(Router);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  public toastService = inject(ToastService);
 
   email: string = '';
   password: string = '';
   isFormValid: boolean = false;
+  isSubmitting:boolean=false;
 
   onEmailChange(): void {
     this.validateForm();
@@ -43,19 +48,27 @@ export class LoginComponent {
     console.log(this.password);
 
     if (!this.isFormValid) {
+      this.toastService.show('Please Fill All Required Fields','danger');
       return;
     }
 
+    if (this.isSubmitting) return;
+    this.isSubmitting=true;
     this.authService.login(this.email, this.password).subscribe({
       next: (response) => {
         if (!response.status) {
+
+          this.toastService.show('Invalid Credentials','danger');
           return;
         }
-
+        console.log("Login Successfully");
+        this.toastService.show('Login Successfully','success');
         this.router.navigate(['/dashboard']);
       },
       error: (error) => {
         console.error('Login error:', error);
+      },complete:()=>{
+        this.isSubmitting=false;
       }
     });
 
