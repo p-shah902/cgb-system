4611d85f618f20e5376b3a6fb21bd138fad72280
Sagar Dudhat <sagardudhat38@gmail.com>
2025-04-03 11:08:33 +0530

    template 3

--- src/app/template3/template3.component.ts
+++ src/app/template3/template3.component.ts
@@ -1,15 +1,365 @@
-import { Component, inject } from '@angular/core';
-import { ToastService } from '../../service/toast.service';
-import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
+import {Component, inject, Renderer2, ViewChild, ElementRef} from '@angular/core';
+import { CKEditorModule, loadCKEditorCloud, CKEditorCloudResult } from '@ckeditor/ckeditor5-angular';
+import type { ClassicEditor, EditorConfig } from 'https://cdn.ckeditor.com/typings/ckeditor5.d.ts';
+import {
+  FormBuilder,
+  FormArray,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+  AbstractControl,
+  ValidationErrors,FormsModule
+} from '@angular/forms';
+import {NgbToastModule} from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
+import {environment} from '../../environments/environment';
+import {Select2} from 'ng-select2-component';
+import {DictionaryService} from '../../service/dictionary.service';
+import {Generalervice} from '../../service/general.service';
+import {UploadService} from '../../service/document.service';
+import {ToastService} from '../../service/toast.service';
+import {Router, ActivatedRoute} from '@angular/router';
+import {DictionaryDetail} from '../../models/dictionary';
+import {UserDetails} from '../../models/user';
+import {UserService} from '../../service/user.service';
+import {PaperService} from '../../service/paper.service';
+import {CountryDetail} from '../../models/general';
+import {PaperStatusType} from '../../models/paper';
+import {VendorService} from '../../service/vendor.service';
+import {VendorDetail} from '../../models/vendor';
+import {CURRENCY_LIST} from '../../utils/constant';
 
 @Component({
   selector: 'app-template3',
   standalone: true,
-  imports: [NgbToastModule,CommonModule],
+  imports: [CommonModule, CKEditorModule, FormsModule, ReactiveFormsModule, Select2, NgbToastModule],
   templateUrl: './template3.component.html',
   styleUrl: './template3.component.scss'
 })
 export class Template3Component {
-  public toastService=inject(ToastService)
+  private readonly userService = inject(UserService);
+  private readonly paperService = inject(PaperService);
+  private readonly vendorService=inject(VendorService);
+  private searchTimeout: any;
+  public Editor: typeof ClassicEditor | null = null;
+  public config: EditorConfig | null = null;
+  @ViewChild('searchInput') searchInput!: ElementRef;
+  generalInfoForm!: FormGroup;
+  isExpanded: boolean = true; // Default expanded
+  paperId: string | null = null;
+  isCopy = false;
+  submitted = false;
+  highlightClass = 'highlight';
+  paperStatusId: number | null = null;
+  paperDetails: any = null;
+  vendorList: VendorDetail[] = []
+  userDetails: UserDetails[] = [];
+  procurementTagUsers: any[] = [];
+  countryDetails: CountryDetail[] = [];
+  paperStatusList: PaperStatusType[] = [];
+
+  // Global variables for dropdown selections
+  currenciesData: DictionaryDetail[] = [];
+  globalCGBData: DictionaryDetail[] = [];
+  operatingFunctionsData: DictionaryDetail[] = [];
+  proposedCMLData: DictionaryDetail[] = [];
+  psaData: DictionaryDetail[] = [];
+  remunerationTypeData: DictionaryDetail[] = [];
+  sourcingRigorData: DictionaryDetail[] = [];
+  sourcingTypeData: DictionaryDetail[] = [];
+  subsectorData: DictionaryDetail[] = [];
+
+  public psaJvOptions = [
+    {value: 'ACG', label: 'ACG'},
+    {value: 'Shah Deniz', label: 'Shah Deniz'},
+    {value: 'SCP', label: 'SCP'},
+    {value: 'BTC', label: 'BTC'},
+    {value: 'Sh-Asiman', label: 'Sh-Asiman'},
+    {value: 'BP Group', label: 'BP Group'}
+  ];
+
+  constructor(private router: Router, private route: ActivatedRoute, private dictionaryService: DictionaryService,
+              private fb: FormBuilder, private countryService: Generalervice, private renderer: Renderer2, private uploadService: UploadService, public toastService: ToastService,
+  ) {
+  }
+
+  public ngOnInit(): void {
+    loadCKEditorCloud({
+      version: '44.3.0',
+      premium: true
+    }).then(this._setupEditor.bind(this));
+
+
+    this.route.paramMap.subscribe(params => {
+      this.paperId = params.get('id');
+      if (this.paperId) {
+      }
+      console.log('Paper ID:', this.paperId);
+    });
+
+    this.route.queryParamMap.subscribe(queryParams => {
+      this.isCopy = queryParams.get('isCopy') === 'true';
+      console.log('Is Copy:', this.isCopy);
+    });
+
+    this.loadForm()
+
+
+  }
+
+  private _setupEditor(cloud: CKEditorCloudResult<{ version: '44.3.0', premium: true }>) {
+    const {
+      ClassicEditor,
+      Essentials,
+      Paragraph,
+      Bold,
+      Italic,
+      Underline,
+      Strikethrough,
+      BlockQuote,
+      Link
+    } = cloud.CKEditor;
+
+    const { FormatPainter } = cloud.CKEditorPremiumFeatures;
+
+    this.Editor = ClassicEditor;
+    this.config = {
+      licenseKey: environment.ckEditorLicenceKey,
+      plugins: [
+        Essentials, Paragraph, Bold, Italic, Underline, Strikethrough,
+        BlockQuote, Link, FormatPainter
+      ],
+      toolbar: [
+        'undo', 'redo', '|', 'bold', 'italic', 'underline', 'strikethrough', '|',
+        'numberedList', 'bulletedList', 'blockquote', 'link', '|', 'formatPainter'
+      ],
+      fontSize: {
+        options: [10, 12, 14, 16, 18, 20, 24, 28, 32, 36],
+        supportAllValues: true
+      },
+      ui: {
+        viewportOffset: { top: 50, bottom: 50 }  // Adjust editor's viewport
+      }
+    };
+  }
+
+
+
+  loadForm() {
+    this.generalInfoForm = this.fb.group({
+      generalInfo: this.fb.group({
+        paperProvision: ['', Validators.required],
+        cgbAtmRefNo: ['', Validators.required],
+        cgbApprovalDate: [{value: '', disabled: true}],
+        isChangeinApproachMarket: [false],
+        cgbItemRefNo: [''],
+        cgbCirculationDate: [''],
+        contractNo: ['', Validators.required],
+        contactNo: ['', Validators.required],
+        vendorId: [null],
+        purposeRequired: ['', Validators.required],
+        globalCGB: ['', Validators.required],
+        bltMember: [null, [Validators.required, Validators.pattern("^[0-9]+$")]],
+        operatingFunction: ['', Validators.required],
+        subSector: ['', Validators.required],
+        sourcingType: ['', Validators.required],
+        camUserId: [null, [Validators.required, Validators.pattern("^[0-9]+$")]],
+        vP1UserId: [null, [Validators.required, Validators.pattern("^[0-9]+$")]],
+        procurementSPAUsers: [[], Validators.required],
+        pdManagerName: [null, Validators.required],
+        isPHCA: [false],
+        currencyCode: [''],
+        totalAwardValueUSD: [0], // Number input
+        exchangeRate: [0], // Number input
+        contractValue: [0],
+        remunerationType: ['', Validators.required],
+        isPaymentRequired: [false],
+        prePayPercent: [0],
+        prePayAmount: [{value: null, disabled: true}],
+        workspaceNo: [''],
+        isSplitAward: [false],
+        psajv: [[], Validators.required],
+        isLTCC: [false],
+        ltccNotes: [''],
+        isGovtReprAligned: [false],
+        govtReprAlignedComment: [''],
+        contractSpendCommitment: [''],
+      }),
+      procurementDetails: this.fb.group({
+        supplierAwardRecommendations: ['', Validators.required],
+        contractStartDate: [''],
+        contractEndDate: [''],
+        extensionOption: [''],
+        isConflictOfInterest: [false],
+        conflictOfInterestComment: [''],
+        isRetrospectiveApproval: [false],
+        retrospectiveApprovalReason: [''],
+        nationalContent: [''],
+      }),
+      ccd: this.fb.group({
+        isHighRiskContract: [false],
+        cddCompleted: [''],
+        highRiskExplanation: [''],
+        flagRaisedCDD: [''],
+        additionalCDD: [''],
+
+      }),
+      evaluationSummary: this.fb.group({
+        invitedBidders: [0],
+        submittedBids: [0],
+        previousContractLearning: [''],
+        performanceImprovements: [''],
+        benchMarking: [''],
+        commericalEvaluation: this.fb.array([]),
+        supplierTechnical: this.fb.array([]),
+      }),
+      additionalDetails: this.fb.group({
+        contractualControls: [''],
+        contractCurrencyLinktoBaseCost: [false],
+        explanationsforBaseCost:[''],
+        riskMitigation: this.fb.array([]),
+      }),
+      valueDelivery: this.fb.group({
+        costReductionPercent: [0],
+        costReductionValue: [0],
+        costReductionRemarks: [''],
+        operatingEfficiencyValue: [0],
+        operatingEfficiencyPercent: [0],
+        operatingEfficiencyRemarks: [''],
+        costAvoidanceValue: [0],
+        costAvoidancePercent: [0],
+        costAvoidanceRemarks: [''],
+      }, {validators: this.requireAllIfAny}),
+      costAllocation: this.fb.group({
+        contractCommittee_SDCC: [{value: false, disabled: true}],
+        contractCommittee_SCP_Co_CC: [{value: false, disabled: true}],
+        contractCommittee_SCP_Co_CCInfoNote: [{value: false, disabled: true}],
+        contractCommittee_BTC_CC: [{value: false, disabled: true}],
+        contractCommittee_BTC_CCInfoNote: [{value: false, disabled: true}],
+        contractCommittee_CGB: [false], //TODO discuss
+        coVenturers_CMC: [{value: false, disabled: true}],
+        coVenturers_SDMC: [{value: false, disabled: true}],
+        coVenturers_SCP: [{value: false, disabled: true}],
+        coVenturers_SCP_Board: [{value: false, disabled: true}],
+        steeringCommittee_SC: [{value: false, disabled: true}],
+        isACG: [{value: false, disabled: true}],
+        isShah: [{value: false, disabled: true}],
+        isSCP: [{value: false, disabled: true}],
+        isBTC: [{value: false, disabled: true}],
+        isAsiman: [{value: false, disabled: true}],
+        isBPGroup: [{value: false, disabled: true}],
+        // Percentage fields with validation (0-100)
+        percentage_isACG: [{value: '', disabled: true}, [Validators.min(0), Validators.max(100)]],
+        percentage_isShah: [{value: '', disabled: true}, [Validators.min(0), Validators.max(100)]],
+        percentage_isSCP: [{value: '', disabled: true}, [Validators.min(0), Validators.max(100)]],
+        percentage_isBTC: [{value: '', disabled: true}, [Validators.min(0), Validators.max(100)]],
+        percentage_isAsiman: [{value: '', disabled: true}, [Validators.min(0), Validators.max(100)]],
+        percentage_isBPGroup: [{value: '', disabled: true}, [Validators.min(0), Validators.max(100)]],
+
+        value_isACG: [{value: '', disabled: true}],
+        value_isShah: [{value: '', disabled: true}],
+        value_isSCP: [{value: '', disabled: true}],
+        value_isBTC: [{value: '', disabled: true}],
+        value_isAsiman: [{value: '', disabled: true}],
+        value_isBPGroup: [{value: '', disabled: true}],
+
+        totalPercentage: [{value: 0, disabled: true}, [Validators.min(0), Validators.max(100)]],
+        totalValue: [{value: 0, disabled: true}]
+      }),
+      costSharing: this.fb.group({
+        isCapex: [false],
+        isFixOpex: [false],
+        isVariableOpex: [false],
+        isInventoryItems: [false],
+        capexMethodology: [{value: '', disabled: true}],
+        fixOpexMethodology: [{value: '', disabled: true}],
+        variableOpexMethodology: [{value: '', disabled: true}],
+        inventoryItemsMethodology: [{value: '', disabled: true}]
+      }),
+      consultation: this.fb.array([]),
+
+    });
+
+  }
+
+  requireAllIfAny(group: AbstractControl): ValidationErrors | null {
+    const validateGroup = (fields: string[]) => {
+      const values = fields.map(field => group.get(field)?.value);
+      const hasValue = values.some(val => val);  // If at least one field has a value
+      const allFilled = values.every(val => val); // If all fields are filled
+
+      return hasValue && !allFilled ? {requireAllFields: true} : null;
+    };
+
+    // Apply validation for each group separately
+    const errors = {
+      costReduction: validateGroup(['costReductionPercent', 'costReductionValue', 'costReductionRemarks']),
+      operatingEfficiency: validateGroup(['operatingEfficiencyPercent', 'operatingEfficiencyValue', 'operatingEfficiencyRemarks']),
+      costAvoidance: validateGroup(['costAvoidancePercent', 'costAvoidanceValue', 'costAvoidanceRemarks'])
+    };
+
+    return Object.values(errors).some(error => error) ? errors : null;
+  }
+
+
+  onSubmit() {
+    console.log("==this.generalInfoForm?.value?", this.generalInfoForm)
+  }
+
+  toggleComments() {
+    this.isExpanded = !this.isExpanded;
+  }
+
+  setPaperStatus(status: string): void {
+    if (!this.paperStatusList?.length) return; // Check if list exists & is not empty
+
+    this.paperStatusId = this.paperStatusList.find(item => item.paperStatus === status)?.id ?? null;
+
+  }
+
+  onSearch(target: EventTarget | null) {
+    if (!target) return;
+
+    const inputElement = target as HTMLInputElement;
+    const searchText = inputElement.value.trim().toLowerCase();
+
+    // Remove old highlights if input is empty
+    if (!searchText) {
+      document.querySelectorAll(`.${this.highlightClass}`).forEach(el => {
+        this.renderer.removeClass(el, this.highlightClass);
+      });
+      return;
+    }
+
+    // Clear previous timeout to debounce
+    clearTimeout(this.searchTimeout);
+
+    // Delay search execution
+    this.searchTimeout = setTimeout(() => {
+      // Remove old highlights
+      document.querySelectorAll(`.${this.highlightClass}`).forEach(el => {
+        this.renderer.removeClass(el, this.highlightClass);
+      });
+
+      // Find label that matches the search text
+      const labels = Array.from(document.querySelectorAll('label'));
+      const matchingLabel = labels.find(label => label.textContent?.toLowerCase().includes(searchText));
+
+      if (matchingLabel) {
+        // Add highlight to the label
+        this.renderer.addClass(matchingLabel, this.highlightClass);
+
+        // Scroll into view
+        matchingLabel.scrollIntoView({behavior: 'smooth', block: 'center'});
+      }
+    }, 500); // Adjust delay as needed
+  }
+  scrollToSection(event: Event) {
+    const selectedValue = (event.target as HTMLSelectElement).value;
+    const section = document.getElementById(selectedValue);
+
+    if (section) {
+      section.scrollIntoView({behavior: 'smooth', block: 'start'});
+    }
+  }
 }
