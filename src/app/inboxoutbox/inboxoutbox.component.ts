c83548e739073cac7481794917f02ad28fb14885
Priyansh Sha <priyansh@mamotechnolabs.com>
2025-04-06 15:11:49 +0530

    Buttons added

--- src/app/inboxoutbox/inboxoutbox.component.ts
+++ src/app/inboxoutbox/inboxoutbox.component.ts
@@ -1,26 +1,39 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {NgbNavModule, NgbToastModule} from '@ng-bootstrap/ng-bootstrap';
+import {Component, inject, OnInit, TemplateRef} from '@angular/core';
+import {NgbDropdownItem, NgbModal, NgbNavModule, NgbToastModule} from '@ng-bootstrap/ng-bootstrap';
 import {ToastService} from '../../service/toast.service';
 import {CommonModule} from '@angular/common';
 import {InboxOutbox} from '../../models/inbox-outbox';
 import {InboxOutboxService} from '../../service/inbox-outbox.service';
 import {RouterLink} from '@angular/router';
+import {LoginUser} from '../../models/user';
+import {AuthService} from '../../service/auth.service';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {PaperConfigService} from '../../service/paper/paper-config.service';
 
 @Component({
   selector: 'app-inboxoutbox',
   standalone: true,
-  imports: [NgbNavModule, NgbToastModule, CommonModule, RouterLink],
+  imports: [NgbNavModule, NgbToastModule, CommonModule, RouterLink, FormsModule],
   templateUrl: './inboxoutbox.component.html',
   styleUrl: './inboxoutbox.component.scss'
 })
 export class InboxoutboxComponent implements OnInit {
 
   public toastService = inject(ToastService)
+
+  private paperConfigService = inject(PaperConfigService);
   active = 1;
   inboxData: InboxOutbox[] = [];
   outboxData: InboxOutbox[] = [];
+  loggedInUser: LoginUser | null = null
+  approvalRemark: string = '';
+  selectedPaper: number = 0;
+  reviewBy: string = '';
 
-  constructor(private inboxOutboxService: InboxOutboxService) {
+  private readonly _mdlSvc = inject(NgbModal);
+
+  constructor(private inboxOutboxService: InboxOutboxService, private authService: AuthService) {
+    this.loggedInUser = this.authService.getUser();
   }
 
   ngOnInit() {
@@ -39,5 +52,51 @@ export class InboxoutboxComponent implements OnInit {
         console.log('ERROR', err);
       }
     })
+  }
+
+  approvePaper(modal: any, type: string) {
+    if (this.selectedPaper > 0) {
+      this.paperConfigService.approveRejectPaper({
+        paperId: this.selectedPaper,
+        remarks: this.reviewBy || '',
+        description: this.approvalRemark,
+        type:
+          this.loggedInUser?.roleName === 'PDM' ? 'PDM Approval' : 'Pre-CGB Approval',
+        check: type,
+      })
+        .subscribe({
+          next: (response) => {
+            if (response.status && response.data) {
+              modal.close('Save click');
+              this.getInboxOutBox();
+            }
+          },
+          error: (error) => {
+            console.log('error', error);
+          },
+        });
+    }
+  }
+
+  open(event: Event, content: TemplateRef<any>, paperId?: number) {
+    event.preventDefault();
+    this._mdlSvc
+      .open(content, {
+        ariaLabelledBy: 'modal-basic-title',
+        centered: true, // Ensure modal is centered
+        size: 'lg', // Adjust size as needed (sm, lg, xl)
+      })
+      .result.then(
+      (result) => {
+        // Handle modal close
+      },
+      (reason) => {
+        // Handle modal dismiss
+      }
+    );
+
+    if (paperId) {
+      this.selectedPaper = paperId;
+    }
   }
 }
