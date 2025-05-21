46776864a9a1ebb0b992be982f69d67b7a99d4df
Priyansh Sha <priyansh@mamotechnolabs.com>
2025-05-21 18:57:27 +0530

    URL updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://cgbwebapi-001-site1.ptempurl.com',
+  apiUrl: 'http://cgbpaper-001-site1.ntempurl.com',
   ckEditorLicenceKey: "eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3NDY2NjIzOTksImp0aSI6ImFjM2E3ZDI1LTZjMDItNDZiMC05NzlmLTAyYTE1NjEwYjcxNiIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiLCJzaCJdLCJ3aGl0ZUxhYmVsIjp0cnVlLCJsaWNlbnNlVHlwZSI6InRyaWFsIiwiZmVhdHVyZXMiOlsiKiJdLCJ2YyI6ImQyMjQ5OTQwIn0.HAtEF3sweBNA0IiKblexR_a03FbOTp014VYSi9O_S7CG26BJAdUcykytiodlh60yaifg74-BFIYSIg2-hYLmMg",
   ckeditorTokenUrl: 'https://mht07l4mn0rt.cke-cs.com/token/dev/b7af7a4f1e6a05ee7fa1ca8e93ae9f036789618c6d01f87ee091dfdaa6ec?limit=10',
   ckeditorSocketUrl: 'wss://12g6463rx0hs.cke-cs.com/ws'
